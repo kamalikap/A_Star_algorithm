2401b5760c8792c8d321979d5b51d63b06d75398
root <root@xbxcloud3.xilinx.com>
2020-05-07 17:04:12 -0700

    Path from start to goal and added 1.board file

--- a_star.cpp
+++ a_star.cpp
@@ -48,9 +48,9 @@ vector<vector<State>> ReadBoardFile(string path) {
 }
 
 //7. Function
-bool Compare(vector<int> node1, vector<int> node2){
-	int f1 = node1[2] + node2[3];
-	int f2 = node1[2] + node2[3];
+bool Compare(const vector<int> node1, const vector<int> node2){
+	int f1 = node1[2] + node1[3];
+	int f2 = node2[2] + node2[3];
 
 	if (f1 > f2){
 		return true;
@@ -79,7 +79,7 @@ int Heuristic(int x1, int y1, int x2, int y2 ){
 bool CheckValidCell(int x, int y, vector<vector<State>> &grid){
 	
 	bool on_grid_x = (x>=0 && x< grid.size());
-	bool on_grid_y = (y>=0 && y<grid.size());
+	bool on_grid_y = (y>=0 && y< grid[0].size());
 	
 	if (on_grid_x  && on_grid_y)
 		return grid[x][y]== State::kEmpty;
@@ -90,23 +90,23 @@ bool CheckValidCell(int x, int y, vector<vector<State>> &grid){
 
 
 //6. Function
-void AddToOpen(int x, int y, int g, int h, vector<vector<int>> open_n, vector<vector<State>> grid){
+void AddToOpen(int x, int y, int g, int h, vector<vector<int>> &open, vector<vector<State>> &grid){
 	vector<int> node {x,y,g,h};
-	open_n.push_back(node);
+	open.push_back(node);
 	grid[x][y] = State::kClosed;
 }
 
 //10. Function
-void ExpandNeighbors(const vector<int> current, int goal[2], vector<vector<int>> &open, vector<vector<State>> grid){
+void ExpandNeighbors(const vector<int> current, int goal[2], vector<vector<int>> &open, vector<vector<State>> &grid){
 	int x = current[0];
 	int y = current[1];
 	int g = current[2];
 
 	for(int i=0; i< 4; i++){
-		int x2 = x+ delta[i][0];
+		int x2 = x + delta[i][0];
 		int y2 = y + delta[i][1];
 		if (CheckValidCell(x2, y2, grid)){
-			int g2 = g+1;
+			int g2 = g + 1;
 			int h2 = Heuristic(x2, y2, goal[0], goal[1]);
 			AddToOpen(x2, y2, g2, h2, open, grid);
 		}
@@ -120,7 +120,7 @@ vector<vector<State>> Search(vector<vector<State>> grid, int start[2], int goal[
 	int y = start[1];
 	int g =0;
 	int h;
-	h= Heuristic(x, y, goal[0], goal[1]);
+	h= Heuristic(x, y, goal[0], goal[1]); //first h=0
 	AddToOpen(x, y, g, h, open, grid);
 	
 	while(open.size() > 0){
@@ -137,22 +137,22 @@ vector<vector<State>> Search(vector<vector<State>> grid, int start[2], int goal[
 		ExpandNeighbors(current, goal, open, grid);
 	}
 	cout << "No path found\n";
-        return vector<vector<State>> {};
+        return vector<vector<State>>{};
 }
 
-
+		
 
 string CellString(State cell ){
 	switch(cell){
 		case State::kObstacle: return "* ";
-		case State::kPath: return "car ";
+		case State::kPath: return "> ";
 		default: return "0 ";
 	}
 }
 
 void PrintBoard(vector<vector<State>> board){
 	for (auto i=0; i<board.size(); i++){
-		for(int j=0; j<board.size(); j++){
+		for(int j=0; j<board[i].size(); j++){
 			cout << CellString(board[i][j]);
 		}
 
